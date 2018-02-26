55762e5ebf96214a52c41f9450e74d4cafba3421
502489615 <yaroslav.novytskyy@syf.com>
2018-02-26 16:25:01 -0500

    - removed unnecessary (duplicating) config;

--- src/main/java/org/yny/sample/gemfire/GemfireConfig.java
+++ src/main/java/org/yny/sample/gemfire/GemfireConfig.java
@@ -2,22 +2,17 @@ package org.yny.sample.gemfire;
 
 import com.gemstone.gemfire.cache.GemFireCache;
 import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
-import com.gemstone.gemfire.pdx.PdxSerializer;
 import com.gemstone.gemfire.pdx.ReflectionBasedAutoSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.gemfire.client.ClientCacheFactoryBean;
 import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
-import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
-import org.springframework.data.gemfire.config.annotation.EnablePdx;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 import org.springframework.data.gemfire.support.ConnectionEndpoint;
 import org.yny.sample.gemfire.repositories.BookRepository;
 
 @Configuration
-@ClientCacheApplication(name = "SpringDataGemfireClientApp")
-@EnablePdx(serializerBeanName = "pdxSerializer") //NB name of the PDX serializer bean
 @EnableGemfireRepositories(basePackageClasses = BookRepository.class)
 public class GemfireConfig {
 
@@ -31,11 +26,6 @@ public class GemfireConfig {
     private String dataModelPackage;
 
     @Bean
-    public PdxSerializer pdxSerializer() {
-        return new ReflectionBasedAutoSerializer();
-    }
-
-    @Bean
     public ClientCacheFactoryBean gemfireCache() {
         ClientCacheFactoryBean clientCacheFactoryBean = new ClientCacheFactoryBean();
         //NB locator has to be configured on the **client** cache factory bean
@@ -46,7 +36,7 @@ public class GemfireConfig {
         return clientCacheFactoryBean;
     }
 
-    @Bean("booksRegion")
+    @Bean("booksRegion") //NB needs to match region name
     public ClientRegionFactoryBean<Object, Object> booksRegion(GemFireCache gemfireCache) {
         ClientRegionFactoryBean<Object, Object> regionFactoryBean = new ClientRegionFactoryBean<>();
         regionFactoryBean.setCache(gemfireCache);
